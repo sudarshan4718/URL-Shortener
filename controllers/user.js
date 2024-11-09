44e20253562c4d8eedb748469a61c817fa0e2512
sudarshan4718 <sudarshanjha2004@gmail.com>
2024-11-09 12:51:59 +0530

    Set up Login page

--- controllers/user.js
+++ controllers/user.js
@@ -10,6 +10,16 @@ async function handleUserSignup (req,res) {
     return res.render("home");
 }
 
+async function handleUserLogin (req,res) {
+    const {email, password} = req.body;
+    const user = await User.findOne({email, password});
+    if(!user) return res.render("login", {
+        error: "Invalid username or password"
+    });
+    return res.redirect("/");
+}
+
 module.exports = {
     handleUserSignup,
+    handleUserLogin,
 }
--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -14,4 +14,8 @@ router.get("/signup", async(req,res) => {
     return res.render("signup");
 })
 
+router.get("/login", async(req,res) => {
+  return res.render("login");
+})
+
 module.exports = router;
